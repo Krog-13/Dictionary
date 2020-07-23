976bea9cc87824dc34ea0e92b9f294f7b2c4aaab
krog-13 <virtualspook@gmail.com>
2020-07-23 15:33:53 +0600

    deleted grapelly

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', include('dict.urls')),
 ]
